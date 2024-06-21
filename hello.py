ac1710e45f763fcc07c763aa7cb23b9c4423ff49
Aiden Cullo <aidencullo@gmail.com>
2024-06-21 11:04:09 -0400

    add options to cli command

--- hello.py
+++ hello.py
@@ -2,5 +2,9 @@ import click
 
 
 @click.command()
-def cli():
-    print("Hello World!")
+@click.option('--string', default='World', help='This is the thing that is greeted')
+@click.option('--repeat', default=1, help='How many times you should be greeted')
+def cli(string, repeat):
+    """This script says hello to the world"""
+    for _ in range(repeat):
+        click.echo("Hello %s!" % string)
