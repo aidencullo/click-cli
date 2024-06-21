bcec7eac956f93fff269f6a6b888486e49ca0e56
Aiden Cullo <aidencullo@gmail.com>
2024-06-21 14:57:19 -0400

    alter and pass ctx to subcommand

--- hello.py
+++ hello.py
@@ -1,30 +1,17 @@
 import click
 
 
-class Config(object):
-
+class State:
     def __init__(self):
-        self.verbose = False
+        self.value = "Initial State"
 
-
-pass_config = click.make_pass_decorator(Config, ensure=True)
 
 @click.group()
-@click.option('--verbose', is_flag=True)
-@pass_config
-def cli(config, verbose):
-    print("cli")
-    config.verbose = verbose
-
+@click.pass_context
+def cli(ctx):
+    ctx.obj = State()
 
 @cli.command()
-@click.option('--string', default='World', help='This is the thing that is greeted')
-@click.option('--repeat', default=1, help='How many times you should be greeted')
-@click.argument('out', type=click.File('w'), default='-', required=False)
-@pass_config
-def say(config, string, repeat, out):
-    """This script says hello to the world"""
-    if config.verbose:
-        click.echo("We are in verbose mode")
-    for _ in range(repeat):
-        click.echo("Hello %s!" % string, file=out)
+@click.pass_obj
+def show(state):
+    click.echo(f"Current state value: {state.value}")
