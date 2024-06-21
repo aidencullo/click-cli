6f4861eb6160de2bde199a1da89187196e03a77e
Aiden Cullo <aidencullo@gmail.com>
2024-06-21 16:26:04 -0400

    hook callback to option

--- hello.py
+++ hello.py
@@ -6,12 +6,24 @@ class State:
         self.value = "Initial State"
 
 
+def my_callback(ctx, param, value):
+    print("ctx.obj", ctx.obj)
+    print("ctx.invoked_subcommand", ctx.invoked_subcommand)
+    print("ctx.command", ctx.command)
+    if value is not None:
+        click.echo(f'Option value is: {value}')
+    else:
+        click.echo('Option not used.')
+
+        
 @click.group()
 @click.pass_context
 def cli(ctx):
+    print("CLI")
     ctx.obj = State()
 
 @cli.command()
+@click.option('--myoption', callback=my_callback, is_flag=True, help='Example option')
 @click.pass_obj
-def show(state):
+def show(state, myoption):
     click.echo(f"Current state value: {state.value}")
