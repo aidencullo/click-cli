2d038486a867fa4e6ad43d63e206c2fb8e8bb944
Aiden Cullo <aidencullo@gmail.com>
2024-06-21 14:28:58 -0400

    add command group and subcommand to the command line interface

--- hello.py
+++ hello.py
@@ -1,10 +1,18 @@
 import click
 
 
-@click.command()
+@click.group()
+@click.option('--verbose', is_flag=True)
+def cli(verbose):
+    if verbose:
+        click.echo('We are in verbose mode')
+
+
+@cli.command()
 @click.option('--string', default='World', help='This is the thing that is greeted')
 @click.option('--repeat', default=1, help='How many times you should be greeted')
-def cli(string, repeat):
+@click.argument('out', type=click.File('w'), default='-', required=False)
+def say(string, repeat, out):
     """This script says hello to the world"""
     for _ in range(repeat):
-        click.echo("Hello %s!" % string)
+        click.echo("Hello %s!" % string, file=out)
