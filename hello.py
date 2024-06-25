5fe1debcf5b9a833ae7df4fc00bfdf4cb8628057
Aiden Cullo <aidencullo@gmail.com>
2024-06-25 14:46:08 -0400

    demonstrating eager option

--- hello.py
+++ hello.py
@@ -1,27 +1,14 @@
 import click
 
-@click.group()
-@click.option('--verbose', is_flag=True, help='Enable verbose mode.')
-@click.option('--help', '-h', is_flag=True, callback=show_help, expose_value=False, is_eager=True, help='Show this message and exit.')
-def cli(verbose):
-    if verbose:
-        click.echo('Verbose mode enabled.')
+def eager(ctx, param, value):
+    print('Eager')
 
-@cli.command()
-def command1():
-    click.echo('Command 1 executed.')
+def lazy(ctx, param, value):
+    print('Lazy')
 
-@cli.command()
-def command2():
-    click.echo('Command 2 executed.')
-
-def show_help(ctx, param, value):
-    if value and not ctx.resilient_parsing:
-        if not ctx.invoked_subcommand:  # Main help
-            click.echo(ctx.get_help())
-        else:  # Sub-command help
-            subcommand = ctx.invoked_subcommand
-            command = ctx.command.commands.get(subcommand)
-            if command:
-                click.echo(command.get_help(ctx))
-        ctx.exit()
+@click.command()
+@click.option('--other', is_flag=True, callback=lazy)
+@click.option('--other', is_flag=True, callback=lazy)
+@click.option('--verbose', is_flag=True, callback=eager)
+def cli(verbose, other):
+    pass
