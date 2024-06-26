d45e71ef4ceba4d241cafbe1ea2e4a5eb68c00b4
Aiden Cullo <aidencullo@gmail.com>
2024-06-26 15:58:12 -0400

    isolate testing condition

--- hello.py
+++ hello.py
@@ -3,9 +3,8 @@ import click
 class PipenvGroup(click.Group):
     def get_help_option(self, ctx):
         def show_help(ctx, param, value):
-            print("Hello, world!")
-            print(ctx.get_help())
-            pass
+            if value and not ctx.invoked_subcommand:
+                click.echo(ctx.get_help())
 
         return click.Option(
             ["-h", "--help"],
@@ -13,9 +12,12 @@ class PipenvGroup(click.Group):
             is_eager=True,
             expose_value=False,
             callback=show_help,
-            help="Show this message and exit.",
         )
 
-@click.command(cls=PipenvGroup, invoke_without_command=True)
+@click.command(cls=PipenvGroup)
 def cli():
-    pass
+    click.echo("cli")
+
+@cli.command()
+def hello():
+    click.echo("Hello World!")
